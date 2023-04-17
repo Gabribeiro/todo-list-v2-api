6103b2e06b1ab238c21b09889d8fed643a086b51
Gabriel Ribeiro <gabrielribeiroreboucas@gmail.com>
2023-04-17 03:36:06 -0300

    Primeiro commit

---
+++ app/Http/Controllers/api/ItemController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers\api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Item;
+
+class ItemController extends Controller
+{
+    public function index()
+    {
+        return Item::all();
+    }
+
+    public function store(Request $request)
+    {
+        try {
+            Item::create($request->all());
+            return Item::all();
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function show($id)
+    {
+        try {
+            return $item = Item::findOrFail($id);;
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function update(Request $request, $id){
+        try {
+            $item = Item::findOrFail($id);
+            $item->update($request->all());
+            return Item::all();
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+    }
+
+    public function destroy($id)
+    {
+        $item = Item::findOrFail($id);
+        try {
+            $item->delete();
+            return Item::all();
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+        
+    }
+}
